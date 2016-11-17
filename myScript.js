fde80757d577af14c600847fe49a5038de207524
AisKrim <alexandros2papastoikos@gmail.com>
2016-11-18 00:30:59 +0200

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -7,7 +7,7 @@ function expandMenu() {
         myContent.style.height = "30%";
         myContent.style.top = "35%";
         myContent.style.left = "35%";
-        document.getElementById("eye").src = "images/closed-eye-icon-v2.png";
+        document.getElementById("eye").src = "closed-eye-icon-v2.png";
         document.getElementById("button").style.backgroundColor = "#20202c";
         myBoolean = true;
     }else{
@@ -17,7 +17,7 @@ function expandMenu() {
         myContent.style.height = "0";
         myContent.style.top = "50%";
         myContent.style.left = "50%";
-        document.getElementById("eye").src = "images/eye-icon-v2.png";
+        document.getElementById("eye").src = "eye-icon-v2.png";
         document.getElementById("button").style.backgroundColor = "#b30000";
         myBoolean = false;
     }
