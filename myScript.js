ee4c80a36b02cb772ffeb095f496576501b235f3
AisKrim <alexandros2papastoikos@gmail.com>
2016-11-19 08:42:35 +0200

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,23 +2,23 @@ var myBoolean = false;
 function expandMenu() {
     if (myBoolean == false){
         var myContent = document.getElementById("wrapContent");
+        document.getElementById("eye").src = "closed-eye-icon-v2.png";
+        document.getElementById("button").style.backgroundColor = "#20202c";
         myContent.style.transitionDuration = "0.3s";
         myContent.style.width = "30%";
         myContent.style.height = "30%";
         myContent.style.top = "35%";
         myContent.style.left = "35%";
-        document.getElementById("eye").src = "closed-eye-icon-v2.png";
-        document.getElementById("button").style.backgroundColor = "#20202c";
         myBoolean = true;
     }else{
         var myContent = document.getElementById("wrapContent");
+        document.getElementById("eye").src = "eye-icon-v2.png";
+        document.getElementById("button").style.backgroundColor = "#b30000";
         myContent.style.transitionDuration = "initial";
         myContent.style.width = "0";
         myContent.style.height = "0";
         myContent.style.top = "50%";
         myContent.style.left = "50%";
-        document.getElementById("eye").src = "eye-icon-v2.png";
-        document.getElementById("button").style.backgroundColor = "#b30000";
         myBoolean = false;
     }
 }
